eecb917d06435dbce36c2652eb70ee0a1dfade10
Wagyourtail <wagyourtail@wagyourtail.xyz>
2023-08-08 20:34:54 -0600

    add comment

--- src/main/java/xyz/wagyourtail/unimined/jarmodagent/JMAInitializer.java
+++ src/main/java/xyz/wagyourtail/unimined/jarmodagent/JMAInitializer.java
@@ -5,6 +5,12 @@ import xyz.wagyourtail.unimined.jarmodagent.transformer.refmap.RefmapBuilder;
 
 public interface JMAInitializer {
 
+    /**
+     * please note this is called within the Java Agent's stuff and won't have access to parts of the classpath.
+     * @param jarModder
+     * @param transformerListBuilder
+     * @param refmapBuilder
+     */
     void jmaMain(JarModder jarModder, TransformerListBuilder transformerListBuilder, RefmapBuilder refmapBuilder);
 
 }
